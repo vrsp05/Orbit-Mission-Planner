cca32aed9d2f7813fe254e53c829f77c21cb560a
Victor Rafael Santana Pimentel <victorrafaelsantana@hotmail.es>
2026-01-26 21:53:28 -0700

    environment testing application

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        // This is the entry point of your application
+        System.out.println("Hello, Orbit Mission Planner!");
+    }
+}
